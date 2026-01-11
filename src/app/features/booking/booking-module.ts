0599e19e5f5d1b283036958e7e68d78c446c489d
AdharshJolly <ajcodesofficial+github@gmail.com>
2026-01-11 10:12:22 +0530

    fix: Correct SharedModule import paths in feature modules

--- src/app/features/booking/booking-module.ts
+++ src/app/features/booking/booking-module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing-module';
-import { SharedModule } from '../../../shared/shared-module';
+import { SharedModule } from '../../shared/shared-module';
 import { BookingForm } from './booking-form/booking-form';
 
 
--- src/app/features/events/events-module.ts
+++ src/app/features/events/events-module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EventsRoutingModule } from './events-routing-module';
-import { SharedModule } from '../../../shared/shared-module';
+import { SharedModule } from '../../shared/shared-module';
 import { EventList } from './event-list/event-list';
 import { EventDetail } from './event-detail/event-detail';
 
